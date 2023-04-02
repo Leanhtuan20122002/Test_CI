484775da43897201f7adc5cfe61da305c0fcd14f
Xuan Dao <daotx@flextech.vn>
2023-04-02 21:06:45 +0700

    commit2

--- src/App.js
+++ src/App.js
@@ -42,32 +42,45 @@ function App() {
       },
     ]
 
+  async function getInfo(){
+    cityName = document.getElementById('input').value;
+    let APIString = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=53d2b9e5ffc4c41135c1487777c28306`;
+    data = await getData(APIString);
+    if (data['cod'] === '404'){
+      document.getElementById('cityName').innerHTML = "";
+      document.getElementById('description').innerHTML = "";
+      document.getElementById('temp').innerHTML = "";
+      document.getElementById('icon').classList.add('notFound');
+      document.getElementById('description').innerHTML = "Not found!";
+    } else {
+      document.getElementById('cityName').innerHTML = data['name'];
+      document.getElementById('description').innerHTML = data['weather'][0]['main'];
+      document.getElementById('temp').innerHTML = data['main']['temp'];
+      const el = document.createElement("div");
+      el.classList.add("degree");
+      el.textContent = "o";
+      document.getElementById('temp').append(el);
+      document.getElementById('temp').append("C");
+      let iconImg = "";
+      ListIcon.forEach(el => {
+        if (data['weather'][0]['main'] === el['type']) iconImg = el['img']
+      })
+      document.getElementById('icon').innerHTML = `<img src='${iconImg}'></img>`;
+    }
+  }
+
   async function inputOnKeyDown(event) {
     if (event.key === "Enter") {
-      cityName = document.getElementById('input').value;
-      let APIString = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=53d2b9e5ffc4c41135c1487777c28306`;
-      data = await getData(APIString);
-      if (data['cod'] === '404'){
-        document.getElementById('cityName').innerHTML = "Not found";
-        document.getElementById('description').innerHTML = "";
-        document.getElementById('temp').innerHTML = "";
-        document.getElementById('icon').innerHTML = "";
-      } else {
-        document.getElementById('cityName').innerHTML = data['name'];
-        document.getElementById('description').innerHTML = data['weather'][0]['main'];
-        document.getElementById('temp').innerHTML = data['main']['temp'];
-        const el = document.createElement("div");
-        el.classList.add("degree");
-        el.textContent = "o";
-        document.getElementById('temp').append(el);
-        document.getElementById('temp').append("C");
-        let iconImg = "";
-        ListIcon.forEach(el => {
-          if (data['weather'][0]['main'] === el['type']) iconImg = el['img']
-        })
-        document.getElementById('icon').innerHTML = `<img src='${iconImg}'></img>`;
-      }
+      document.getElementById('main').classList.add('showAni');
+      setInterval(() => {
+        getInfo();
+      }, 1000);
     }
+  }
+
+  function searchOnClick(){
+    document.getElementById('main').classList.add('showAni');
+    getInfo();
   }
 
   async function getData(api){
@@ -82,9 +95,10 @@ function App() {
 
   return (
     <div className="App">
-      <div className="main">
+      <div className="main" id='main'>
         <div className='search-bar'>
           <input id='input' onKeyDown={inputOnKeyDown}></input>
+          <div className='serch-icon' onClick={searchOnClick}></div>
         </div>
         <div className="city-name" id='cityName'></div>
         <div className="icon" id='icon'></div>
